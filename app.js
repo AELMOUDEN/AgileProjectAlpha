3c4c8ca0b1101ec947aa2dff4ac9567c0770bf16
AELMOUDEN <ahmed_elmouden3@um5.ac.ma>
2023-05-13 18:22:13 +0100

    changed DB from local to MongoAtlas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const Client = require("./models/client");
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/clientDB", {
+mongoose.connect("mongodb+srv://ahmedelmouden:n7UtzoLrIxOLpAe3@agilemongocluster.wusfibn.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -13,6 +13,35 @@ db.on("error", console.error.bind(console, "Connection Error:"));
 db.once("open", () => {
   console.log("Database Connected");
 });
+
+// i know, i know, db password leaked!
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://ahmedelmouden:n7UtzoLrIxOLpAe3@agilemongocluster.wusfibn.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
 
 app.use(express.urlencoded({ extended: true }));
 
